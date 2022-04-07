1f87136e7f3218e2e250f7ed5154c3d387867894
rssmith6 <89939030+rssmith6@users.noreply.github.com>
2022-04-07 14:32:55 -0500

    Update database.php

--- admin/model/database.php
+++ admin/model/database.php
@@ -1,9 +1,10 @@
 <?php
-$dsn = "mysql:host=localhost;dbname=zippyusedautos";
-$username = 'root';
+$dsn = "mysql:host=kfgk8u2ogtoylkq9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=ygdlmxex3jvt9v67";
+$username = 'tnu86ovjrc4uj06o';
+$password = 'ig021061rrg2kc73';
 
 try{
-    $db = new PDO($dsn, $username);
+    $db = new PDO($dsn, $username, $password);
 } catch(PDOException $e) {
     $error_message = 'Database Error: ';
     $error_message .= $e->getMessage();
